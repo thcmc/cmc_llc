d437d3338f25532a1eef1ceaec8247c9d7c6948c
tmh144429 <tmh144429@gmail.com>
2014-05-14 16:18:35 -0400

    initial version

---
+++ config/env/all.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+	app: {
+		title: 'CMC - Custom Web Apps',
+		description: 'Web Application Development',
+		keywords: 'mongodb, express, angularjs, node.js, mongoose, passport, pittsburgh, developer, applications'
+	},
+	port: process.env.PORT || 3000,
+	templateEngine: 'swig',
+	sessionSecret: 'MEAN',
+	sessionCollection: 'sessions',
+	assets: {
+		lib: {
+			css: [
+				'public/lib/bootstrap/dist/css/bootstrap.css',
+				'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+				'http://fonts.googleapis.com/css?family=Share+Tech+Mono',
+				'http://fonts.googleapis.com/css?family=Arvo',
+				'http://fonts.googleapis.com/css?family=Ewert',
+				'http://fonts.googleapis.com/css?family=Electrolize'
+			],
+			js: [
+				'public/lib/angular/angular.js',
+				'public/lib/angular-resource/angular-resource.js',
+				'public/lib/angular-animate/angular-animate.js',
+				'public/lib/angular-ui-router/release/angular-ui-router.js',
+				'public/lib/angular-ui-utils/ui-utils.js',
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js'
+			]
+		},
+		css: [
+			'public/modules/**/css/*.css'
+		],
+		js: [
+			'public/config.js',
+			'public/application.js',
+			'public/modules/*/*.js',
+			'public/modules/*/*[!tests]*/*.js'
+		],
+		tests: [
+			'public/lib/angular-mocks/angular-mocks.js',
+			'public/modules/*/tests/*.js'
+		]
+	}
+};
---
+++ config/env/development.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+	db: 'mongodb://localhost/mean-dev',
+	app: {
+		title: 'CMC - Custom Web Apps'
+	},
+	facebook: {
+		clientID: process.env.FACEBOOK_ID || '317110068443898',
+		clientSecret: process.env.FACEBOOK_SECRET || '5693dd6194e1abd20f7666f1ae4b0207',
+		callbackURL: 'http://localhost:3000/auth/facebook/callback'
+	},
+	twitter: {
+		clientID: process.env.TWITTER_KEY || 'CONSUMER_KEY',
+		clientSecret: process.env.TWITTER_SECRET || 'CONSUMER_SECRET',
+		callbackURL: 'http://localhost:3000/auth/twitter/callback'
+	},
+	google: {
+		clientID: process.env.GOOGLE_ID || 'APP_ID',
+		clientSecret: process.env.GOOGLE_SECRET || 'APP_SECRET',
+		callbackURL: 'http://localhost:3000/auth/google/callback'
+	},
+	linkedin: {
+		clientID: process.env.LINKEDIN_ID || 'APP_ID',
+		clientSecret: process.env.LINKEDIN_SECRET || 'APP_SECRET',
+		callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+	}
+};
