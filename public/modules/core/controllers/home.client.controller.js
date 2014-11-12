a46f01f358a64cca5ac048aa987034cebd9e054e
thcmc <th@creativemonsterconsulting.com>
2014-11-12 13:53:38 -0500

    initial

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -3,6 +3,21 @@
 angular.module('core').controller('HomeController', ['$scope', 'Authentication', function ($scope, Authentication) {
     $scope.authentication = Authentication;
 
+    // var code = document.getElementById('code');
+        var demo = document.getElementById('demo');
+        var svg = document.getElementsByTagName('svg')[0];
+
+        // Convert the SVG node to HTML.
+        var div = document.createElement('div');
+        div.appendChild(svg.cloneNode(true));
+
+        // Encode the SVG as base64
+        var b64 = 'data:image/svg+xml;base64,'+window.btoa(div.innerHTML);
+        var url = 'url("' + b64 + '")';
+        // code.innerHTML = 'Base64 CSS (for cross-browser compatibility)\n\nbackground-image: ' + url + ';';
+        demo.style.backgroundImage = url;
+
+
     $scope.projects = [
     	{company:'East End Eatz',url:'http://www.eastendeatz.com',logo:'#',desc:'#', t1:'Node.JS',t2:'Angular.JS',t3:'#',t4:'#',t5:'#',img:'modules/core/img/ss_eee.png'},
     	{company:'CYA',url:'http://www.catholicyouthassociation.com',logo:'#',desc:'#', t1:'Node.JS',t2:'#',t3:'#',t4:'#',t5:'#',img:'modules/core/img/ss_cya.png'},
